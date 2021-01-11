f03be6af1a3749471a49391da5667616c00be36b
Viko1 <vitaliiropiak@gmail.com>
2021-01-11 23:33:29 +0100

    React config

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage"
+import RoomCreatePage from "./RoomCreatePage"
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom"
+
+
+export default class HomePage extends Component{
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <Router>
+            <Switch>
+               <Route exact path='/'>This is the Home page</Route>
+               <Route path='/join' component={RoomJoinPage}/>
+               <Route path='/create' component={RoomCreatePage}/>
+            </Switch>
+         </Router>
+      )
+   }
+}
---
+++ frontend/src/components/RoomCreatePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class RoomCreatePage extends Component{
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return <p>This is a Room Create page page</p>
+   }
+}
